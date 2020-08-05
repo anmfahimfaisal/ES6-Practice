6976fe93c955ff589aa83e19b88da7510ca1ace5
eisenham <anmfahimfaisal@gmail.com>
2020-08-06 01:44:58 +0600

    Practice day 01

---
+++ letconst.js
@@ -0,0 +1,21 @@
+const guy1 = "David";
+
+console.log(guy1);
+
+const numbers = [12, 45]
+numbers[1] = 88;
+numbers.push(12);
+//const players = {name: "Mich", phone: 674 33 290, age: 23};
+console.log(numbers);
+
+let patientName = "Flinn";
+patientName = "Clarke";
+console.log(patientName);
+
+let sum = 0;
+for (let i = 0; i < 10;i++) {
+    sum = sum+i;  
+
+}
+
+console.log(i);
---
+++ template.js
@@ -0,0 +1,21 @@
+const firstName = "Justin";
+const lastName = "Timberlake";
+
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName} is a good man`;
+const fullName3 = `${20+30+50}`;
+// const multiLine = "I was here.\n"
+//     +"Before the sunset.\n"
+//     +"Before the world."
+
+
+// multiline in ES6
+const multiLine2 = `I was here.
+I had the time.
+I could bring it in.`
+
+console.log(fullName);
+console.log(fullName2);
+console.log(fullName3);
+//console.log(multiLine);
+console.log(multiLine2);
